617c59e0ac05738ac16fff0dfe4f022c48a2a52c
Aaron Moraes <147651798+AaronMoraes@users.noreply.github.com>
2024-06-10 16:21:52 -0300

    Linguagem C-Exercicio 11.c

---
+++ Linguagem C-Exercicio 11.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int main() {
+  int A[10], B[10], i;
+  
+  printf("VETOR A \n");
+  for (i=0; i<11; ++i)
+ {
+     printf("A[%i]:", i);
+     scanf("%d", &A[i]);
+ }
+ 
+ printf("\nIMPRESSÃO VETOR A\n");
+   for (i=0; i<11; ++i)
+   {
+     printf("A[%i]: %d\n",i,A[i]);
+   }   
+   
+  printf("\nIMPRESSÃO VALOR B\n");
+  for(i=0; i<11; ++i)
+  {
+      printf("B[%i]: %d\n", i,A[i]);
+  }
+
+    return 0;
+}
